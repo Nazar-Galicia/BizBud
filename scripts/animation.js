61178e7ea6d94741869f3c5e746f814649632848
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-02-24 17:15:00 +0200

    feat: add intersection observer callback and options

---
+++ scripts/animation.js
@@ -0,0 +1,17 @@
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.01,
+}
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target)
+
+            setTimeout(() => {
+                entry.target.classList.remove('show');
+            }, 1500)
+        }
+    })
+}

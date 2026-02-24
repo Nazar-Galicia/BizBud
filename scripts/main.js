fe2b4a297a68018326e2c1a0bdc50130287e8b20
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-02-24 17:33:08 +0200

    feat: add animation by scrolling page

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+import { scrollAnimation } from "./animation.js";
+
+const sectionHeadings = document.querySelectorAll(".section-heading"); // 3
+const sectionSubtitles = document.querySelectorAll(".section-subtitle"); // 2
+const servicesCards = document.querySelectorAll(".services__card"); // all
+const statsListItems = document.querySelectorAll(".stats__list-item"); // all
+const aboutUsParagraphs = document.querySelectorAll(".about-us__text");
+const heroHeading = document.querySelector(".hero__heading");
+const heroParagraph = document.querySelector(".hero__paragraph");
+const heroButton = document.querySelector(".hero > .button");
+
+const animatedElements = [
+    sectionHeadings[0],
+    sectionHeadings[1],
+    sectionHeadings[2],
+    sectionSubtitles[0],
+    sectionSubtitles[1],
+    heroHeading,
+    heroParagraph,
+    heroButton,
+    ...servicesCards,
+    ...statsListItems,
+    ...aboutUsParagraphs,
+];
+
+console.log(animatedElements)
+
+scrollAnimation(...animatedElements);

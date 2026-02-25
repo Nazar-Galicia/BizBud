267c7116b52b3e439d7b6d7e3883b36301d7d3b5
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-02-25 18:33:44 +0200

    style: add animation for opening and closing navigation menu

--- scripts/open-nav-menu.js
+++ scripts/open-nav-menu.js
@@ -1,7 +1,20 @@
 const burger = document.querySelector('.header__burger-button');
+const headerNavigation = document.querySelector('.header__navigation');
 
 const toggleNavMenu = () => {
     burger.classList.toggle('cross');
+
+    if (!headerNavigation.classList.contains('open')) {
+        headerNavigation.classList.add('open');
+    } else {
+        headerNavigation.classList.add('close');
+
+        setTimeout(() => {
+            headerNavigation.classList.remove('close');
+            headerNavigation.classList.remove('open');
+        }, 270)
+    }
+
 }
 
 export { burger, toggleNavMenu };
